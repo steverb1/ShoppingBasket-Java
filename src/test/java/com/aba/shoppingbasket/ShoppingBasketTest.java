e14b125b43bd2794daf822960a5501a4422d20bb
steve.bement <steve.bement@accenture.com>
2022-05-13 09:50:56 -0600

    Adding Junit5

--- src/test/java/com/aba/shoppingbasket/ShoppingBasketTest.java
+++ src/test/java/com/aba/shoppingbasket/ShoppingBasketTest.java
@@ -1,5 +1,12 @@
 package com.aba.shoppingbasket;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class ShoppingBasketTest {
-	
+	@Test
+	public void test1() {
+		assertTrue(true);
+	}
 }
